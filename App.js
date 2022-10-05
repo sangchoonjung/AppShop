b7e0b7583ec8819cdc62654b812115a2c3737d0f
sangchoonjung <109258408+sangchoonjung@users.noreply.github.com>
2022-10-05 15:48:13 +0900

    22-10-05

--- App.js
+++ App.js
@@ -1,11 +1,18 @@
+import { useFonts } from 'expo-font';
 import { StatusBar } from 'expo-status-bar';
 import { StyleSheet, Text, View } from 'react-native';
 
 export default function App() {
+  const [fontsLoaded] = useFonts({
+    'baseFont': require("./assets/font/NanumGothic-Regular.ttf")
+  })
+  if (!fontsLoaded) {
+    return <StatusBar/>
+  }
   return (
     <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
       <StatusBar style="auto" />
+      <Text style={{fontFamily:"baseFont",fontSize:40}}>상춘Open up App.js to start working on your app!</Text>
     </View>
   );
 }

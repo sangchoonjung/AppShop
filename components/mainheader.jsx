a032ac77eda304144a0968e450e4b8bbd8a96b2a
sangchoonjung <109258408+sangchoonjung@users.noreply.github.com>
2022-10-17 15:22:55 +0900

    1017 헤더수정

--- components/mainheader.jsx
+++ components/mainheader.jsx
@@ -1,21 +1,31 @@
 import { useNavigation } from "@react-navigation/native";
 import { Button, StyleSheet, Text, View } from "react-native";
 import BaseFont from "../assets/font/base";
-
+import { Ionicons } from "@expo/vector-icons";
 function MainHeader({ back }) {
-
   const navigation = useNavigation();
-
-
 
   const goBackHandle = () => {
     navigation.goBack();
-  }
+  };
   return (
     <View style={styles.main}>
-      {!back && <Button onPress={goBackHandle} title="back" style={styles.button} />}
-      {/* 화살표 아이콘?으로 대체하고 위치조정 필요 */}
-      <BaseFont style={{ fontSize: 20 }}>Beauty Shop</BaseFont>
+      {!back && (
+        <View>
+          <Ionicons
+            name="arrow-back-outline"
+            size={24}
+            color="#006699"
+            onPress={goBackHandle}
+          />
+        </View>
+      )}
+
+      <View>
+        <BaseFont style={{ fontSize: 20}}>
+          Beauty Shop
+        </BaseFont>
+      </View>
     </View>
   );
 }
@@ -24,14 +34,11 @@ const styles = StyleSheet.create({
     width: "100 %",
     height: 80,
     backgroundColor: "#A5D8FA",
-    justifyContent: "center",
-    alignItems: "center",
     paddingTop: 10,
-
-    //---------------------------
     flexDirection: "row",
+    justifyContent: "center",
+  alignItems:"center"
   },
-  button: {},
 });
 
 export default MainHeader;
